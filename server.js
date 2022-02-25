d9a47b8aaf313a73e971ca3921dee01fa1961d62
Stephanie <stephanieanderson5133@gmail.com>
2022-02-25 13:10:36 -0600

    end of lesson 2

--- server.js
+++ server.js
@@ -64,9 +64,13 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
-    
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id) [0];
+    return result;
+  }
+
 app.get('/api/animals', (req, res) => {
-    let results = animals;
+    const results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
@@ -74,5 +78,14 @@ app.get('/api/animals', (req, res) => {
 });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
